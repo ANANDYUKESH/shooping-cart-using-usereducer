2aa18110f76aa535528398610ba6c0335e3528b8
YUKESH <123308359+ANANDYUKESH@users.noreply.github.com>
2024-08-23 00:06:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,123 +1,58 @@
 import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
-import {useState,useReducer} from 'react';
+import { useReducer } from 'react';
 
 import Shirt from './image/n5.jpg';
-import Shirt1 from './image/n6.jpg';
 import Shirt2 from './image/n7.jpg';
 import Shirt3 from './image/n8.jpg';
 
-
-
 const initialState = { count: 0 };
-
-
-
-
-function Welcome(state, action) {
+function welcome(state, action) {
   switch (action.category) {
     case 'increment':
       return { count: state.count + 1 };
     case 'decrement':
-      return { count: state.count - 1 };
-    case 'reset': 
+      return { count: state.count > 0 ? state.count - 1 : 0 };
+    case 'reset':
       return { count: 0 };
     default:
-      throw new Error();
-  }
+       throw new Error("Error occurs");
+           }
 }
 
-const Shop = () => {
-    const [state, dispatch] = React.useReducer(Welcome, initialState);
+const SingleProduct = ({ imgSrc, product, price }) => {
+  const [state, dispatch] = useReducer(welcome, initialState);
 
-  return (
-    <div>
+  return (<>
+    <div className='content'>
+      <div className='product'>
+        <img src={imgSrc} alt={product} />
+        <h1>{product}</h1>
+        <h3>₹ {price}</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
 
-        <div className="container">
-
-            <h1 className='text-center' >Happy Shopy</h1>
-
-
-           <div className="row">
-                <div className="col">
-                <div class="card " >
-                <img src={Shirt} alt=""  class="card-img-top"/>
-                <div class="card-body">
-                    <h5 class="card-title"></h5>
-                    <p class="card-text"></p>
-                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">
-                    <button onClick={() => dispatch({ category: 'increment' })}>+</button>
-                    <input type="text" value={ state.count} className='w-25 mx-3 text-center' />
-                    <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
-                    </li>
-                    <li class="list-group-item">
-                       <b> Rate:</b>₹300
-                    </li>
-                </ul>
-               
-            </div>
-
-                                        
-
-                </div>
-                <div className="col">
-                <div class="card " >
-                <img src={Shirt1} alt=""  class="card-img-top"/>
-                <div class="card-body">
-                    <h5 class="card-title"></h5>
-                    <p class="card-text"></p>
-                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">
-                    <button onClick={() => dispatch({ category: 'increment' })}>+</button>
-                    <input type="text" value={ state.count} className='w-25 mx-3 text-center' />
-                    <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
-                    </li>
-                    <li class="list-group-item">
-                       <b> Rate:</b>₹120/Kg
-                    </li>
-                </ul>
-               
-            </div>
-
-                </div>
-                <div className="col">
-                <div class="card " >
-                <img src={Shirt2} alt=""  class="card-img-top"/>
-                <div class="card-body">
-                    <h5 class="card-title"></h5>
-                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Double pocket shirt</li>
-                    <li class="list-group-item">
-                    <button onClick={() => dispatch({ category: 'increment' })}>+</button>
-                    <input type="text" value={ state.count} className='w-25 mx-3 text-center' />
-                    <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
-                    </li>
-                   
-                </ul>
-               
-            </div>
-
-                </div>
-           </div>
-
-
-
-
-        </div>
-
-
-
-
-
-      
+      <div className="cart">
+        <button onClick={() => dispatch({ category: 'increment' })}>+</button>
+        <input type="number" name="count" value={state.count} readOnly />
+        <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
+        <button onClick={() => dispatch({ category: 'reset' })}>Reset </button>
+      </div>
+      <h2>Total: ₹{state.count * price}</h2>
     </div>
-  )
+    </>
+  );
 }
+const Shop = () => {
+  return (
+    <div className='App'>
+      <SingleProduct imgSrc={Shirt} product="Denim Shirt" price={500} />
+      <SingleProduct imgSrc={Shirt2} product="Double pocket Shirt" price={700} />
+      <SingleProduct imgSrc={Shirt3} product="Party Shirt" price={800} />
+    </div>
+  );
+}
+
 export default Shop;
+
